dd83031e9f26dbf646fa39383a1aef9e9e00d9b5
Etom <98264570+SageCoda@users.noreply.github.com>
2024-04-10 18:39:53 +0100

    server add 2

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,27 +1,46 @@
 // import { useNavigate } from "react-router-dom";
 import classes from "./Authpage.module.css";
 import { useEffect, useState } from "react";
-// import Cookies from "universal-cookie";
+import Cookies from "universal-cookie";
 
 export default function Login() {
   const[email,setEmail]= useState('');
   const [password, setPassword] = useState('');
-  const[loading,setLoading]=useState(false)
-  
+  const [loading, setLoading] = useState(false)
+  const [error,setError]=useState('')
+  const cookies = new Cookies()
   const loginHandler = async () => {
-        setLoading(true);
+    try {
+      setLoading(true);
 
-    const data = {
-      email: email,
-      password:password
+      const data = {
+        email: email,
+        password: password
+      }
+      // setTimeout(() => {
+      //   setLoading(false)
+      // }, 3000)
+      const response = await fetch('http://localhost:1234/login/', {
+        method: 'POST',
+        body: JSON.stringify(data)
+      })
+      const responseData = await response.json()
+      if (!response.ok) {
+        setError(responseData.message)
+        console.log(responseData.message);
+        return
+      }
+      cookies.set('user-id',response.user)
+
     }
-    // setTimeout(() => {
-    //   setLoading(false)
-    // }, 3000)
-    const response = await fetch('http://localhost:1234/login/', {
-      method: 'POST',
-      body:JSON.stringify(data)
-    })
+        catch (e) {
+      
+    }
+    finally {
+      
+    }
+     
+    
     setLoading(false);
 
   }
@@ -64,6 +83,7 @@ export default function Login() {
             color: "red",
           }}
         >
+          {error && error}
         </p>
         <div className={classes.signup}>
           <span className={classes.signup}>
